e0a5a09952a64c318d7e368ccef10f4dd2550d93
adminmytourmitra-creator <admin.mytourmitra@gmail.com>
2026-08-11 13:03:23 +0530

    Update agencies.js

--- js/modules/agencies.js
+++ js/modules/agencies.js
@@ -1,1 +1,7 @@
+// ======================================================
+// B2B AGENCIES MODULE
+// ======================================================
 
+export function initAgencies() {
+  console.log("B2B Agencies module initialized");
+}
