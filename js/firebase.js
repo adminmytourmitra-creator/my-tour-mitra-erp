2b9326fe71dbc9ada18e9dd941175a13690ced82
adminmytourmitra-creator <admin.mytourmitra@gmail.com>
2026-08-11 12:47:43 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,31 @@
+// ======================================================
+// MY TOUR MITRA ERP
+// FIREBASE INITIALIZATION
+// ======================================================
 
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+import { firebaseConfig } from "./config.js";
+
+
+// ======================================================
+// INITIALIZE FIREBASE
+// ======================================================
+
+const app = initializeApp(firebaseConfig);
+
+
+// ======================================================
+// FIREBASE SERVICES
+// ======================================================
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
