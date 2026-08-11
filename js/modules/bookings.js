0a3802d1c0890e380dd95dfff4801f815a230ea7
adminmytourmitra-creator <admin.mytourmitra@gmail.com>
2026-08-11 13:06:42 +0530

    Update bookings.js

--- js/modules/bookings.js
+++ js/modules/bookings.js
@@ -1,1 +1,7 @@
+// ======================================================
+// BOOKINGS MODULE
+// ======================================================
 
+export function initBookings() {
+  console.log("Bookings module initialized");
+}
