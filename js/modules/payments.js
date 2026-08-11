cbda1e54151a4ab3c9d7073ec565139253610e44
adminmytourmitra-creator <admin.mytourmitra@gmail.com>
2026-08-11 13:11:06 +0530

    Update payments.js

--- js/modules/payments.js
+++ js/modules/payments.js
@@ -1,1 +1,7 @@
+// ======================================================
+// PAYMENTS MODULE
+// ======================================================
 
+export function initPayments() {
+  console.log("Payments module initialized");
+}
