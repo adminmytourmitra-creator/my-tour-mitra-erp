772b7842d1aae90e0f6b50c9c1ad37f86da88a9e
adminmytourmitra-creator <admin.mytourmitra@gmail.com>
2026-08-11 12:46:44 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,25 @@
+// ======================================================
+// MY TOUR MITRA ERP
+// FIREBASE CONFIGURATION
+// ======================================================
 
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyA7yV96KZ5w4tjRy2c_bwtFYTu8r-mNzU4",
+
+  authDomain:
+    "my-tour-mitra-erp.firebaseapp.com",
+
+  projectId:
+    "my-tour-mitra-erp",
+
+  storageBucket:
+    "my-tour-mitra-erp.firebasestorage.app",
+
+  messagingSenderId:
+    "104596097197",
+
+  appId:
+    "1:104596097197:web:7abcc59c6feee02fe05e24"
+
+};
