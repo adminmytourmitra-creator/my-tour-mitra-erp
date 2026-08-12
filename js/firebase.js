24c5723fe9cc57fadb35a2d046df2edf9786dbaa
adminmytourmitra-creator <admin.mytourmitra@gmail.com>
2026-08-12 16:00:29 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,27 @@
 // =====================================================
 // FIREBASE INITIALIZATION
+// MY TOUR MITRA ERP
 // =====================================================
 
-import { initializeApp } from
-  "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
-import { getAuth } from
-  "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-import { getFirestore } from
-  "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-import { firebaseConfig } from
-  "./config.js";
+import {
+  getStorage
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
 
+import {
+  firebaseConfig
+} from "./config.js";
 
 // =====================================================
 // INITIALIZE FIREBASE
@@ -23,7 +31,6 @@ const app =
   initializeApp(
     firebaseConfig
   );
-
 
 // =====================================================
 // FIREBASE SERVICES
@@ -35,6 +42,8 @@ const auth =
 const db =
   getFirestore(app);
 
+const storage =
+  getStorage(app);
 
 // =====================================================
 // EXPORT SERVICES
@@ -43,5 +52,6 @@ const db =
 export {
   app,
   auth,
-  db
+  db,
+  storage
 };
